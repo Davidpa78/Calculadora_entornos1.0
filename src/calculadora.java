b0ba6e8997ec268ef147119aa6c9ea97dc93cd88
Davidpa78 <davidpa7899@gmail.com>
2018-02-28 23:25:54 +0100

    Añado comentarios para aclarar el programa en los puntos clave

--- src/calculadora.java
+++ src/calculadora.java
@@ -4,10 +4,12 @@ import java.util.Scanner;
 public class calculadora {
 	
 	private static Scanner reader;
-	
+
+	/*Separamos en funciones para simplificar el código y hacerlo más legible*/
+
 	public static double raiz(double op1,double op2) {
 		
-		return Math.pow(op1,1/op2);
+		return Math.pow(op1,1/op2);/*Para hacer la raiz al no haber ninguna función establezco que el exponente es 1/op2*/
 	}
 
 	public static double potencia(double op1, double op2) {
@@ -43,6 +45,7 @@ public class calculadora {
         double op2;
         double resultado;
 
+        /*Creamos un Scanner para dar valor a las variables a través del usuario*/
         reader = new Scanner(System.in);
        
 
@@ -53,9 +56,10 @@ public class calculadora {
         System.out.println("Introduce el segundo operando(Potencias: Exponente , Raices: Índice):");
         op2 = reader.nextDouble();
         System.out.println("Introduce el nombre de una operación: ");
-        
+
+        /*Creamos un switch para dar valor a resultado en función de la operación que escriba*/
         switch (reader.next()){
-        	case"raiz":resultado=raiz(op1,op2);
+        	case"raiz":resultado=raiz(op1,op2); /*Llamada a las funciones de la operación*/
         	break;
         	case"potencia":resultado=potencia(op1,op2);
         	break;
@@ -67,13 +71,14 @@ public class calculadora {
                 break;
             case"division":resultado=division(op1,op2);
                 break;
+                /*Si el usuario no introduce una operación que se encuentre en el switch se muestra por pantalla un mensaje y termina el programa*/
             default:
                 System.out.println("Operacion no reconocida");
                 return;
       
 
         }
-
+        /*Imprimimos resultado*/
         System.out.println("El resultado es "+ resultado);
 
 
