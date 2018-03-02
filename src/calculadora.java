55bd73a83f99f25a6fa98bb95ed9c0509d17a189
KelvinJoelC <Kelvinjcarreno@gmail.com>
2018-03-02 14:18:08 +0100

    Mejora visual y comprobación de errores.

--- src/calculadora.java
+++ src/calculadora.java
@@ -7,39 +7,37 @@ public class calculadora {
 
 	/*Separamos en funciones para simplificar el código y hacerlo más legible*/
 
-	public static double raiz(double op1,double op2) {
+	public static double suma(double op1, double op2) {
+            
+        return op1+op2;
+        
+        }
+        public static double resta(double op1, double op2) {
+            
+        return op1-op2;
+        
+        }
+        public static double multiplicacion(double op1, double op2) {
+
+        return op1*op2;
+
+    }
+        public static double division(double op1, double op2) {
+
+        return op1/op2;
+
+        }
+        public static double raiz(double op1,double op2) {
 		
 		return Math.pow(op1,1/op2);/*Para hacer la raiz al no haber ninguna función establezco que el exponente es 1/op2*/
 	}
-
 	public static double potencia(double op1, double op2) {
 		
 		return Math.pow(op1,op2);
 	}
 
-    public static double suma(double op1, double op2) {
-
-      return op1+op2;
-
-    }
-    public static double resta(double op1, double op2) {
-
-        return op1-op2;
-
-    }
-
-    public static double multiplicacion(double op1, double op2) {
-
-        return op1*op2;
-
-    }
-    public static double division(double op1, double op2) {
-
-        return op1/op2;
-
-    }
-
-    public static void main(String[] args) {
+     
+        public static void main(String[] args) {
 
         double op1;
         double op2;
@@ -50,30 +48,36 @@ public class calculadora {
        
 
 
-        System.out.println("Introduce el primer operando(Potencias: Base , Raices: Radicando):");
+        System.out.println("Introduce el primer operando:");
         op1 = reader.nextDouble();
 
-        System.out.println("Introduce el segundo operando(Potencias: Exponente , Raices: Índice):");
+        System.out.println("Introduce el segundo operando:");
         op2 = reader.nextDouble();
-        System.out.println("Introduce el nombre de una operación: ");
+        System.out.println("Introduce el numero de la operación deseada: \n"
+                + "1: Suma\n"
+                + "2: Resta\n"
+                + "3: Multiplicación\n"
+                + "4: Divión\n"
+                + "5: Raíz\n"
+                + "6: Potencias\n");
 
         /*Creamos un switch para dar valor a resultado en función de la operación que escriba*/
         switch (reader.next()){
-        	case"raiz":resultado=raiz(op1,op2); /*Llamada a las funciones de la operación*/
+        	case"1":resultado=suma(op1,op2); /*Llamada a las funciones de la operación*/
         	break;
-        	case"potencia":resultado=potencia(op1,op2);
+        	case"2":resultado=resta(op1,op2);
         	break;
-            case"suma":resultado=suma(op1,op2);
+                case"3":resultado=multiplicacion(op1,op2);
                 break;
-            case"resta":resultado=resta(op1,op2);
+                case"4":resultado=division(op1,op2);
                 break;
-            case"multiplicacion":resultado=multiplicacion(op1,op2);
+                case"5":resultado=raiz(op1,op2);
                 break;
-            case"division":resultado=division(op1,op2);
+                case"6":resultado=potencia(op1,op2);
                 break;
                 /*Si el usuario no introduce una operación que se encuentre en el switch se muestra por pantalla un mensaje y termina el programa*/
             default:
-                System.out.println("Operacion no reconocida");
+                System.out.println("Solo puedes introducir numeros del 1-6");
                 return;
       
 
@@ -81,6 +85,10 @@ public class calculadora {
         /*Imprimimos resultado*/
         System.out.println("El resultado es "+ resultado);
 
+// Errores a modificar 
+//Poner un limite de caracteres y restriccion de caracteres alfabéticos
+//No se puede dividir entre 0  , op2 =/ 0
+//Raiz op1 no puede ser negativo, y op2 solo puede tomar valores >1
 
 
 
